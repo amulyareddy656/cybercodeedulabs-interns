8d8f3312101bf300c3b338d1bb334d5f29fca892
amulyareddy656 <amulyareddy656@gmail.com>
2026-06-03 14:04:40 +0000

    Classifying event type

--- amulya-log-monitor/log_monitor.py
+++ amulya-log-monitor/log_monitor.py
@@ -1,4 +1,13 @@
 LOG_FILE="/var/log/auth.log"
 file=open(LOG_FILE,"r")
-print(file.read())
+for line in file:
+	if "Failed password for root" in line:
+		print("ROOT_ATTACK | HIGH")
+		print(line)
+	elif "Failed password for" in line or "Invalid user" in line:
+		print("SSH_FAILURE | MEDIUM")
+		print(line)
+	elif "Accepted  password" in line or "Accepted publicly" in line:
+		print("LOGIN_SUCCESS | LOW")
+		print(line)
 file.close()
