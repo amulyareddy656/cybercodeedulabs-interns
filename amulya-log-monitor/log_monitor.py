1c7c9e0c301308dbb142de5a8030f379bd0cf429
amulya656 <amulyareddy656@gmail.com>
2026-06-11 15:59:07 +0000

    Adding brute force counter to log_monitor.py script

--- amulya-log-monitor/log_monitor.py
+++ amulya-log-monitor/log_monitor.py
@@ -12,6 +12,7 @@ def extract_user(line):
 		match=re.search(pattern,line)
 		if match:
 			return match.group(1)
+ip_failure={}
 try:
 	file=open(LOG_FILE,"r")
 	file.seek(0,2)
@@ -38,7 +39,21 @@ try:
 		else:
 			continue
 		timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-		print(f"[{timestamp}] {event_type} | {severity} | IP: {ip} | User: {user}")
+		if event_type=="SSH_FAILURE":
+			key=extract_ip(line)
+			value=datetime.now()
+			if key not in ip_failure:
+				ip_failure[key]=[]
+			ip_failure[key].append(value)
+			while ip_failure[key]:
+				timestamp_difference=value-ip_failure[key][0]
+				if(timestamp_difference.total_seconds()>=60):
+					ip_failure[key].pop(0)
+				else:
+					break
+			if len(ip_failure[key])>=5:
+				print(f"[{timestamp}] BRUTE_FORCE | HIGH | IP : {ip} | 5 failures in 60 seconds")
+		#print(f"[{timestamp}] {event_type} | {severity} | IP: {ip} | User: {user}")
 except KeyboardInterrupt:
 	print("Stop monitoring")
 finally:
