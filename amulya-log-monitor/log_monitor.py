4fdc7290c2edbcf85a0e6d25ca67cbed74690aa4
amylya656 <amulyareddy656@gmail.com>
2026-06-18 08:51:37 +0000

    Added port scan detection logic and priority scoring(based on the type of event by it's weights and frequencies

--- amulya-log-monitor/log_monitor.py
+++ amulya-log-monitor/log_monitor.py
@@ -1,46 +1,137 @@
 import re
 import time
 from datetime import datetime
-LOG_FILE="/var/log/auth.log"
+
+auth_log="/var/log/auth.log"
+sys_log="/var/log/syslog"
+
 def extract_ip(line):
 	ip_match=re.search(r'(\d+\.\d+\.\d+\.\d+)',line)
 	if ip_match:
 		return ip_match.group(1)
+
 def extract_user(line):
 	patterns=[ r'for (\w+)' ]
 	for pattern in patterns:
 		match=re.search(pattern,line)
 		if match:
 			return match.group(1)
+
+def detect_ufw(line):
+        if "[UFW BLOCK]" not in line:
+                return None
+        src_match=re.search(r'SRC=(\d+\.\d+\.\d+\.\d+)',line)
+        dpt_match=re.search(r'DPT=(\d+)',line)
+        if src_match and dpt_match:
+                return src_match.group(1), dpt_match.group(1)
+        return None
+
+def record_event(event_type,severity,ip):
+        global highest_severity
+        global highest_event
+        event_count[event_type]=(event_count.get(event_type,0)+1)
+        ip_count[ip]=(ip_count.get(ip,0)+1)
+        if severity_rank[severity] > severity_rank[highest_severity]:
+                highest_severity=severity
+                highest_event=event_type
+        if event_type in kill_chain:
+                if ip not in ip_data:
+                        ip_data[ip] ={"events":[], "stages":set() }
+                ip_data[ip]["events"].append(event_type)
+                ip_data[ip]["stages"].add(kill_chain[event_type])
+
+def calculate_score(ip):
+	score=0
+	events=ip_data[ip]["events"]
+	for event in set(events):
+		count=events.count(event)
+		weight=attack_weights[event]
+		if count<=3:
+			multiplier=1
+		elif count<=9:
+			multiplier=1.5
+		else:
+			multiplier=2
+		score=score+(weight*count*multiplier)
+	if len(ip_data[ip]["stages"])>=2:
+		score+=10
+	if score<=10:
+		priority="LOW"
+		recommendation="Continue monitoring"
+	elif score <=25:
+		priority="MEDIUM"
+		recommendation="Continue monitoring"
+	elif score<=50:
+		priority="HIGH"
+		recommendation="Monitor closely"
+	else:
+		priority="Critical"
+		recommendation="Block the IP immediately"
+	return score,priority,recommendation
+
 ip_failure={}
+event_count = {}
+ip_count = {}
+highest_severity = "LOW"
+highest_event = "None"
+severity_rank = {
+    "LOW": 1,
+    "MEDIUM": 2,
+    "HIGH": 3,
+    "CRITICAL": 4
+}
+port_scan_dict = {}
+ip_data = {}
+kill_chain = {
+    "PORT_SCAN": "RECONNAISSANCE(1)",
+    "SSH_FAILURE": "DELIVERY(3)",
+    "BRUTE_FORCE": "DELIVERY(3)",
+    "ROOT_ATTACK": "EXPLOITATION(4)"
+}
+attack_weights = {
+    "PORT_SCAN": 1,
+    "SSH_FAILURE": 3,
+    "BRUTE_FORCE": 5,
+    "ROOT_ATTACK": 8
+}
+
 try:
-	file=open(LOG_FILE,"r")
-	file.seek(0,2)
+	auth_file=open(auth_log,"r")
+	auth_file.seek(0,2)
+	sys_file=open(sys_log,"r")
+	sys_file.seek(0,2)
+	last_report=time.time()
+
 	while True:
-		line=file.readline()
-		if not line:
+		auth_line=auth_file.readline()
+		sys_line=sys_file.readline()
+		if not auth_line and not sys_line:
 			time.sleep(0.5)
 			continue
-		if "Failed password for root" in line:
+		if "Failed password for root" in auth_line:
 			event_type="ROOT_ATTACK"
 			severity="HIGH"
-			ip=extract_ip(line)
-			user=extract_user(line)
-		elif "Failed password for" in line or "Invalid user" in line:
+			ip=extract_ip(auth_line)
+			user=extract_user(auth_line)
+			record_event(event_type,severity,ip)
+		elif "Failed password for" in auth_line or "Invalid user" in auth_line:
 			event_type="SSH_FAILURE"
 			severity="MEDIUM"
-			ip=extract_ip(line)
-			user=extract_user(line)
-		elif "Accepted password" in line or "Accepted publickey" in line:
+			ip=extract_ip(auth_line)
+			user=extract_user(auth_line)
+			record_event(event_type,severity,ip)
+		elif "Accepted password" in auth_line or "Accepted publickey" in auth_line:
 			event_type="LOGIN_SUCCESS"
 			severity="LOW"
-			ip=extract_ip(line)
-			user=extract_user(line)
+			ip=extract_ip(auth_line)
+			user=extract_user(auth_line)
 		else:
 			continue
 		timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		print(f"[{timestamp}] {event_type} | {severity} | IP : {ip} | User : {user}")
+
 		if event_type=="SSH_FAILURE":
-			key=extract_ip(line)
+			key=extract_ip(auth_line)
 			value=datetime.now()
 			if key not in ip_failure:
 				ip_failure[key]=[]
@@ -52,9 +143,53 @@ try:
 				else:
 					break
 			if len(ip_failure[key])>=5:
-				print(f"[{timestamp}] BRUTE_FORCE | HIGH | IP : {ip} | 5 failures in 60 seconds")
-		#print(f"[{timestamp}] {event_type} | {severity} | IP: {ip} | User: {user}")
+				event_type="BRUTE_FORCE"
+				severity="HIGH"
+				record_event(event_type,severity,ip)
+				print(f"[{timestamp}] {event_type} | {severity} | IP : {ip} | 5 failures in 60 seconds")
+
+		if sys_line:
+			result=detect_ufw(sys_line)
+			if result:
+				src_ip,dpt=result
+				if src_ip not in port_scan_dict:
+					port_scan_dict[src_ip]=set()
+				port_scan[src_ip].add(dpt)
+				if dpt=="22":
+					severity="HIGH"
+				elif dpt in ["3306" , "5432"]:
+					severity="CRITICAL"
+				else:
+					severity="MEDIUM"
+				event_type="UFW_BLOCK"
+				print(f"[{timestamp}] {event_type} | Severity : {severity} | SRC_IP : {src_ip} | DPT : {dpt}")
+				record_event(event_type,severity,src_ip)
+				if len(port_scn_dict[src_ip])>=3:
+					event_type="PORT_SCAN"
+					severity="MEDIUM"
+					print(f"[{timestamp}] {event_type} | Severity : {severity} | IP : {src_ip}")
+					record_event(event_type,severity,src_ip)
+
+		if time.time()-last_report>=60:
+			print("\nSUMMARY\n")
+			print("Total event types")
+			for event,count in event_count.items():
+				print(f"{event} : {count}")
+			print("\nTop 3 attacking IPs")
+			temp=ip_count.copy()
+			for i in range(min(3,len(temp))):
+				max_ip=max(temp,key=temp.get)
+				print(max_ip,temp[max_ip])
+				del temp[max_ip]
+			print("\n Highest severity event")
+			print(highest_event," | ",highest_severity)
+			for ip in ip_data:
+				score,priority,recommendation=calculate_score(ip)
+				print(f"IP : {ip} | Score : {score} | Priority : {priority} | Recommendation : {recommendation}")
+			last_report=time.time()
+
 except KeyboardInterrupt:
 	print("Stop monitoring")
 finally:
-	file.close()
+	auth_file.close()
+	sys_file.close()
